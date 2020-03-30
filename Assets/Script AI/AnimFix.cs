e577f902f2da28431146e01c6bda8ade9bd31dda
NivGarbelly <60030777+NivGarbelly@users.noreply.github.com>
2020-03-30 16:30:39 +0300

    V1 AI
    Added AI + Models for player and Teddy bear + Animations

---
+++ Assets/Script AI/AnimFix.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimFix : MonoBehaviour
+{
+    [SerializeField]private Animator animator;
+    private bool R1 = false;
+    void Hit()
+    {
+
+    }
+
+    void Land()
+    {
+        
+    }
+
+    private void Update()
+    {
+        void Update()
+        {
+            animator.SetBool("R1",R1);
+            
+            if (Input.GetMouseButtonDown(0))
+            {
+                animator.SetTrigger("RM");
+                R1 = true;
+            }
+        }
+    }
+}
